49e52cbfdc8cc01d4ab6882794531b364e647a19
mohammad hossein lashgari <m.h.lashgari98@gmail.com>
2022-02-13 12:13:50 +0330

    feat: add header

---
+++ components/header/Header.js
@@ -0,0 +1,49 @@
+/* This example requires Tailwind CSS v2.0+ */
+const navigation = [
+  { name: "SSG", href: "#" },
+  { name: "SSR", href: "#" },
+  { name: "Shop", href: "#" },
+];
+
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-green-400 to-blue-500">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-3 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-10 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                alt=""
+              />
+            </a>
+            <div className="hidden ml-10 space-x-8 lg:block">
+              {navigation.map((link) => (
+                <a
+                  key={link.name}
+                  href={link.href}
+                  className="text-base font-medium text-white hover:text-indigo-50"
+                >
+                  {link.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+          {navigation.map((link) => (
+            <a
+              key={link.name}
+              href={link.href}
+              className="text-base font-medium text-white hover:text-indigo-50"
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+}
